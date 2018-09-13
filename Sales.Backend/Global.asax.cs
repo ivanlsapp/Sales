f34b37be899b066bdb8af875e1722034288b7890
ivanlsapp <ivanlsapp@users.noreply.github.com>
2018-09-14 01:32:41 +0200

    Tutorial 56 - Parte 12 - Migración de la base de datos
    Crear dos nuevos campos y poner la BD preparada para las Auto Migraciones.

--- Sales.Backend/Global.asax.cs
+++ Sales.Backend/Global.asax.cs
@@ -9,7 +9,9 @@
     {
         protected void Application_Start()
         {
-            Database.SetInitializer(new MigrateDatabaseToLatestVersion<Models.LocalDataContext, Migrations.Configuration>());
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<Models.LocalDataContext, 
+                Migrations.Configuration>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
